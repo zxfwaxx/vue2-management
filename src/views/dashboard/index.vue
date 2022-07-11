f3209d8d10347e1bfae7c0ea42558cb339fdaaa8
zxf <1536486716@qq.com>
2022-07-11 20:03:40 +0800

    新增测试

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -14,19 +14,41 @@
       @coma="coma"
       @comb="comb"
     />
-
+    <div class="record">
+      <h1>{{tipMsg}}</h1>
+      <button @click="onStartVoice">开始</button>
+      <button @click="onEndVoice">结束</button>
+      <button @click="onPlayAudio">播放</button>
+      <div
+        class="record-play"
+        v-show="isFinished"
+      >
+        <h2>Current voice player is:</h2>
+        <audio
+          id="audioVoice"
+          controls
+          autoplay
+        ></audio>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import comA from './comA.vue'
+import Record from './js/b';
 export default {
   //import引入的组件需要注入到对象中才能使用
   components: { comA },
   data () {
     //这里存放数据
     return {
-      con: 'ssss'
+      con: 'ssss',
+      isVoice: false,
+      isFinished: false,
+      tipMsg: '录音',
+      audio: "",
+      recorder: new Record()
     };
   },
   //监听属性 类似于data概念
@@ -58,6 +80,49 @@ export default {
     },
     comb (val) {
       console.log(val, '222')
+    },
+    // 开始录音
+    onStartVoice () {
+      this.onStopAudio()
+      this.isFinished = false;
+      this.recorder.startRecord({
+        success: res => {
+          this.isVoice = true
+          console.log(res, '22')
+        },
+        error: e => {
+          this.isVoice = false
+          this.$toast(e)
+        }
+      });
+    },
+
+    // 结束录音
+    onEndVoice () {
+      this.isFinished = false;
+      this.recorder.stopRecord({
+        success: res => {
+          this.isVoice = false
+          //此处可以获取音频源文件(res)，用于上传等操作
+          console.log('音频源文件', res)
+        },
+        error: () => {
+          this.isVoice = false
+        }
+      });
+    },
+
+    // 播放录音
+    onPlayAudio () {
+      this.isVoice = false
+      this.isFinished = true;
+      this.audio = document.getElementById("audioVoice");
+      this.recorder.play(this.audio);
+    },
+
+    // 停止播放录音
+    onStopAudio () {
+      this.recorder.clear(this.audio);
     }
 
   },
